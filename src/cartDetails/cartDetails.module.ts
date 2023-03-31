ebad4c74c748cecbde01669dde3fea4b71b632a2
Anthony <antonyeung919@gmail.com>
2023-03-31 16:36:40 +0800

    edit

--- src/cartDetails/cartDetails.module.ts
+++ src/cartDetails/cartDetails.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { CartDetailsService } from './cartDetails.service';
 import { CartDetailsController } from './cartDetails.controller';
 import { PrismaService } from 'nestjs-prisma';
+import { JwtAuthGuard } from 'utils/jwt-auth.guard';
+import { JwtStrategy } from 'src/users/strategy/jwt.strategy';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [CartDetailsController],
-  providers: [CartDetailsService, PrismaService],
+  providers: [CartDetailsService, PrismaService,JwtService,JwtStrategy],
 })
 export class CartDetailsModule {}
--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { CartsService } from './carts.service';
 import { PrismaService } from 'nestjs-prisma';
 import { CartsController } from './carts.controller';
+import { JwtAuthGuard } from 'utils/jwt-auth.guard';
+import { JwtStrategy } from 'src/users/strategy/jwt.strategy';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [CartsController],
-  providers: [CartsService, PrismaService],
+  providers: [CartsService, PrismaService, JwtService, JwtStrategy],
 })
 export class CartsModule {}
