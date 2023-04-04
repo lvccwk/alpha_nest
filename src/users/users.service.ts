8053dfaa137fbd546a5bf19a034b088e5b1513bb
Lawrence <lawrence_@live.hk>
2023-04-04 15:25:17 +0800

    update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -6,6 +6,7 @@ import { User } from './entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { Users } from '@prisma/client';
+import * as jwtSimple from 'jwt-simple';
 
 @Injectable()
 export class UsersService {
@@ -149,5 +150,59 @@ export class UsersService {
 				image: data.image
 			}
 		});
+	}
+
+	async loginWithFacebook(code: string): Promise<string> {
+		if (!code) {
+			throw new UnauthorizedException('Wrong Code!');
+		}
+		const data = await this.getFacebookAccessToken(code);
+		if (!data.access_token) {
+			throw new UnauthorizedException('Failed to get access token!');
+		}
+		const profileData = await this.getFacebookProfileData(data.access_token);
+		let user: User = await this.findByEmail(profileData.email);
+		if (!user) {
+			user = await this.create({
+				user_type: 'student',
+				username: profileData.name,
+				email: profileData.email,
+				password: '',
+				image: profileData.picture.data.url,
+				is_deleted: false
+			});
+		}
+		const payload = {
+			id: user.id,
+			username: user.username,
+			email: user.email,
+			image: user.image
+		};
+		const token = jwtSimple.encode(payload, process.env.JWT_SECRET);
+		return token;
+	}
+
+	async getFacebookAccessToken(code: string): Promise<any> {
+		const fetchResponse = await fetch(`https://graph.facebook.com/oauth/access_token`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: new URLSearchParams({
+				grant_type: 'authorization_code',
+				client_id: `${process.env.FACEBOOK_CLIENT_ID}`,
+				client_secret: `${process.env.FACEBOOK_CLIENT_SECRET}`,
+				code: `${code}`,
+				redirect_uri: `${process.env.REACT_PUBLIC_HOSTNAME}/facebook-callback`
+			})
+		});
+		return await fetchResponse.json();
+	}
+
+	async getFacebookProfileData(accessToken: string): Promise<any> {
+		const profileResponse = await fetch(
+			`https://graph.facebook.com/me?fields=id,name,email,picture&access_token=${accessToken}`
+		);
+		return await profileResponse.json();
 	}
 }
