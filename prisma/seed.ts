7855ed9a2478fbb76c76c12827704ae2a27f54b7
Lawrence <lawrence_@live.hk>
2023-04-18 19:21:31 +0800

    update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -50,7 +50,70 @@ async function main() {
 			}
 		]
 	});
-	console.log(fixedSubject);
+
+	const arfarteacher2 = await prisma.users.upsert({
+		where: { email: 'arfar1@gmail.com' },
+		update: {},
+		create: {
+			user_type: 'teacher',
+			username: '牙花',
+			email: 'arfar1@gmail.com',
+			password: 'adminadmin',
+			image: 'https://alphafile.s3.ap-southeast-1.amazonaws.com/001-002.png',
+			teacher: {
+				create: {
+					info: '中大中大中大中大中大中大中大中大中大',
+					school: '中大',
+					experience: 1,
+					product: {
+						create: [
+							{
+								name: '中文精讀班',
+								price: 20,
+								product_type: 'course',
+
+								file_url:
+									'https://alphafile.s3.ap-southeast-1.amazonaws.com/%E6%88%AA%E5%9C%96+2023-04-06+19.22.05.png',
+								image: 'https://alphafile.s3.ap-southeast-1.amazonaws.com/001-002.png',
+								subject_id: 1,
+								info: '中大大爆炸'
+							},
+							{
+								name: '中文精讀筆記',
+								price: 30,
+								product_type: 'note',
+
+								file_url:
+									'https://alphafile.s3.ap-southeast-1.amazonaws.com/%E6%88%AA%E5%9C%96+2023-04-06+19.22.05.png',
+								image: 'https://alphafile.s3.ap-southeast-1.amazonaws.com/001-002.png',
+								subject_id: 1,
+								info: '中大大爆炸'
+							}
+						]
+					}
+				}
+			}
+		}
+	});
+
+	const arfarstudent1 = await prisma.users.upsert({
+		where: { email: 'arfar2@gmail.com' },
+		update: {},
+		create: {
+			user_type: 'student',
+			username: '牙花花',
+			email: 'arfar2@gmail.com',
+			password: 'adminadmin',
+			image: 'https://alphafile.s3.ap-southeast-1.amazonaws.com/001-002.png',
+			purchase_history: {
+				create: {
+					product_id: 1
+				}
+			}
+		}
+	});
+
+	console.log(fixedSubject, arfarteacher2);
 }
 main()
 	.then(async () => {
